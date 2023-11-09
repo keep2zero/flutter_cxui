ccfe9b5d87eac97fb9966ef46cbaa7a90c7d5ddb
keep2zero <towaywu@gmail.com>
2023-11-09 22:30:42 +0800

    feat: 组件化ImageCard

--- lib/components/image_card/image_card.dart
+++ lib/components/image_card/image_card.dart
@@ -1,15 +1,138 @@
 import 'package:flutter/material.dart';
 
 class ImageCard extends StatefulWidget {
-  const ImageCard({super.key});
+  ImageCard({
+    super.key,
+    this.title,
+    this.subtitle,
+    this.topRight,
+    this.topLeft,
+    this.bottomRight,
+    this.bottomLeft,
+    this.cornerHeight,
+  });
 
   @override
   State<ImageCard> createState() => _ImageCardState();
+  List<Widget>? topLeft;
+  List<Widget>? topRight;
+  List<Widget>? bottomLeft;
+  List<Widget>? bottomRight;
+  String? title;
+  String? subtitle;
+  double? cornerHeight;
 }
 
 class _ImageCardState extends State<ImageCard> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      // margin: const EdgeInsets.all(5),
+      padding: const EdgeInsets.all(5),
+      // color: Colors.black,
+      child: Stack(
+        // fit: StackFit.expand,
+        children: [
+          buildMain(),
+        ],
+      ),
+    );
+  }
+
+  Widget buildCover() {
+    return Expanded(
+      child: Stack(
+        children: [
+          Container(
+            height: double.infinity,
+            // padding: EdgeInsets.all(8),
+
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(5),
+              child: const Image(
+                width: double.infinity,
+                fit: BoxFit.cover,
+                image: AssetImage("assets/img/card-img-01.jpg"),
+              ),
+            ),
+          ),
+          buildTopRight(),
+          buildTopLeft(),
+          buildBottomRight(),
+          buildBottomLeft()
+        ],
+      ),
+    );
+  }
+
+  Column buildMain() {
+    final children = [buildCover()];
+    if (widget.title != null || widget.subtitle != null) {
+      children.add(
+        const SizedBox(
+          height: 8,
+        ),
+      );
+    }
+
+    if (widget.title != null) {
+      children.add(
+        Text(
+          widget.title!,
+          style: const TextStyle(
+            fontSize: 16,
+          ),
+        ),
+      );
+    }
+
+    if (widget.subtitle != null) {
+      children.add(
+        Text(
+          widget.subtitle!,
+          style: const TextStyle(
+            fontSize: 14,
+            color: Colors.black54,
+          ),
+        ),
+      );
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: children,
+    );
+  }
+
+  Widget buildRound(double? left, double? top, double? right, double? bottom,
+      List<Widget> children) {
+    return Positioned(
+      right: right,
+      top: top,
+      left: left,
+      bottom: bottom,
+      child: Container(
+          // color: Colors.green,
+          height: widget.cornerHeight ?? 20,
+          child: Row(
+            children: children,
+          )),
+    );
+  }
+
+  Widget buildTopRight() {
+    return buildRound(null, 0, 0, null, widget.topRight ?? []);
+  }
+
+  Widget buildTopLeft() {
+    return buildRound(0, 0, null, null, widget.topLeft ?? []);
+  }
+
+  Widget buildBottomRight() {
+    return buildRound(null, null, 0, 0, widget.bottomRight ?? []);
+  }
+
+  Widget buildBottomLeft() {
+    return buildRound(0, null, null, 0, widget.bottomLeft ?? []);
   }
 }
--- lib/pages/page_image_card.dart
+++ lib/pages/page_image_card.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_cxui/components/image_card/image_card.dart';
 
 class PageImageCard extends StatelessWidget {
   const PageImageCard({super.key});
@@ -25,50 +27,66 @@ class PageImageCard extends StatelessWidget {
   }
 
   Widget? itemBuilder(BuildContext context, int index) {
-    return Container(
-      margin: EdgeInsets.all(5),
-      padding: EdgeInsets.all(5),
-      // color: Colors.black,
-      child: Stack(
-        // fit: StackFit.expand,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: Container(
-                  // padding: EdgeInsets.all(8),
-
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(5),
-                    child: const Image(
-                      width: double.infinity,
-                      fit: BoxFit.cover,
-                      image: AssetImage("assets/img/card-img-01.jpg"),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 8,
-              ),
-              Text(
-                "清风落叶",
-                style: TextStyle(
-                  fontSize: 16,
-                ),
-              ),
-              Text(
-                "拽姐英子一秒黑脸",
-                style: TextStyle(
-                  fontSize: 14,
-                  color: Colors.black54,
-                ),
-              ),
-            ],
+    return ImageCard(
+      title: "清风落叶",
+      subtitle: "又一个神剧",
+      topLeft: const [
+        SizedBox(
+          width: 8,
+        ),
+        Icon(
+          Icons.hotel_class,
+          color: Colors.amber,
+        ),
+      ],
+      topRight: [
+        Container(
+          decoration: const BoxDecoration(
+            color: Colors.green,
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(5),
+              bottomLeft: Radius.circular(5),
+            ),
           ),
-        ],
-      ),
+          padding: const EdgeInsets.only(left: 5, right: 5),
+          height: double.infinity,
+          child: const Center(
+            child: Text(
+              "独播",
+              style: TextStyle(
+                fontSize: 12,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        )
+      ],
+      bottomRight: const [
+        Text(
+          "16集全",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        SizedBox(width: 5),
+      ],
+      bottomLeft: [
+        Container(
+          // margin: EdgeInsets.all(5),
+          // padding: EdgeInsets.all(5),
+
+          decoration: const BoxDecoration(
+            color: Colors.purple,
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(5),
+            ),
+          ),
+          child: const Text(
+            "电视机",
+            style: TextStyle(color: Colors.white, fontSize: 12),
+          ),
+        )
+      ],
     );
   }
 }
