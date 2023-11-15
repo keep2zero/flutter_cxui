af5b42d9f1daf9c1325a69ea220908064ce72b96
keep2zero <towaywu@gmail.com>
2023-11-15 11:11:22 +0800

    feat: 调整示例界面

---
+++ lib/data/place_grid_data.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_cxui/components/place_grid/place_grid.dart';
+
+List<PlaceGridItem> pgi_01 = [
+  PlaceGridItem(
+    icon: Icons.list_alt,
+    title: "预约 | 加追",
+    iconSize: 26,
+    // color: ,
+  ),
+  PlaceGridItem(
+    icon: Icons.download_for_offline_outlined,
+    title: "我的下载",
+    iconSize: 26,
+    // color: gcolor,
+  ),
+  PlaceGridItem(
+    icon: Icons.video_collection_outlined,
+    title: "我的片单",
+    iconSize: 26,
+    // color: gcolor,
+    // color: Colors.purple,
+  ),
+  PlaceGridItem(
+    icon: Icons.star_border,
+    title: "我的收藏",
+    // color: Colors.pink,
+    // color: gcolor,
+    iconSize: 26,
+  ),
+];
+List<PlaceGridItem> pgi_02 = [
+  PlaceGridItem(
+    icon: Icons.list_alt,
+    title: "预约 | 加追",
+    iconSize: 26,
+    // color: gcolor,
+  ),
+  PlaceGridItem(
+    icon: Icons.download_for_offline_outlined,
+    title: "我的下载",
+    iconSize: 26,
+    // color: gcolor,
+  ),
+  PlaceGridItem(
+    icon: Icons.video_collection_outlined,
+    title: "我的片单",
+    iconSize: 26,
+    // color: gcolor,
+    // color: Colors.purple,
+  ),
+  PlaceGridItem(
+    icon: Icons.star_border,
+    title: "我的收藏",
+    // color: Colors.pink,
+    // color: gcolor,
+    iconSize: 26,
+  ),
+];
--- lib/pages/page_place_grid.dart
+++ lib/pages/page_place_grid.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_cxui/components/card/card.dart';
 import 'package:flutter_cxui/components/place_grid/place_grid.dart';
+import 'package:flutter_cxui/config.dart';
+import 'package:flutter_cxui/data/place_grid_data.dart';
 
 class PagePlaceGrid extends StatelessWidget {
   const PagePlaceGrid({super.key});
@@ -13,57 +15,29 @@ class PagePlaceGrid extends StatelessWidget {
         padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
-            card01(null, null, null, null),
-            const SizedBox(
+            CxCard(
+              bgColor: CxConfig.white,
+              radius: 16,
+              body: CxPlaceGrid(
+                cols: 4,
+                items: pgi_01,
+              ),
+            ),
+            SizedBox(
               height: 20,
             ),
-            card01(Colors.white, "宫格例子", Colors.black, Colors.black),
+            CxCard(
+              radius: 16,
+              hdBgColor: CxConfig.white,
+              bgColor: CxConfig.white,
+              title: "更多功能",
+              body: CxPlaceGrid(
+                cols: 4,
+                items: pgi_02,
+              ),
+            ),
           ],
         ),
-      ),
-    );
-  }
-
-  CxCard card01(
-      Color? bgColor, String? title, Color? titleColor, Color? gcolor) {
-    return CxCard(
-      bgColor: bgColor,
-      title: title,
-      titleColor: titleColor,
-
-      // titleSize: 14,
-      body: CxPlaceGrid(
-        // height: 20,
-        //  ratio: 1,
-        cols: 4,
-        items: [
-          PlaceGridItem(
-            icon: Icons.list_alt,
-            title: "预约 | 加追",
-            iconSize: 26,
-            color: gcolor,
-          ),
-          PlaceGridItem(
-            icon: Icons.download_for_offline_outlined,
-            title: "我的下载",
-            iconSize: 26,
-            color: gcolor,
-          ),
-          PlaceGridItem(
-            icon: Icons.video_collection_outlined,
-            title: "我的片单",
-            iconSize: 26,
-            color: gcolor,
-            // color: Colors.purple,
-          ),
-          PlaceGridItem(
-            icon: Icons.star_border,
-            title: "我的收藏",
-            // color: Colors.pink,
-            color: gcolor,
-            iconSize: 26,
-          ),
-        ],
       ),
     );
   }
