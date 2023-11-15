e0b3088dd04019cf07dcd50ddc689c3293024dc4
keep2zero <towaywu@gmail.com>
2023-11-15 10:49:03 +0800

    feat: 加入更多颜色

--- lib/config.dart
+++ lib/config.dart
@@ -3,5 +3,7 @@ import 'dart:ui';
 class CxConfig {
   static Color primary = const Color.fromARGB(255, 109, 96, 224); //109, 96, 224
   static Color foreground = const Color(0xFFFFFFFF);
-  static Color background = Color.fromARGB(255, 246, 246, 253);
+  static Color background = const Color.fromARGB(255, 246, 246, 253);
+  static Color white = const Color.fromARGB(255, 255, 255, 255);
+  static Color grey = const Color.fromARGB(216, 255, 255, 255);
 }
