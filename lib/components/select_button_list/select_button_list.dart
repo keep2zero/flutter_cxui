dc0be2d9db184e8dc18fa46309067f9051f33b4e
keep2zero <towaywu@gmail.com>
2023-11-16 10:47:10 +0800

    opt: 优化select_button_list

--- lib/components/select_button_list/select_button_list.dart
+++ lib/components/select_button_list/select_button_list.dart
@@ -6,21 +6,27 @@ class CxSelectButtonList extends StatefulWidget {
     required this.data,
     required this.onChange,
     this.size,
-    this.boxColor = Colors.black,
+    this.fontSize,
+    this.boxColor = Colors.transparent,
     this.color = Colors.white,
-    this.bgColor = Colors.white38,
+    this.bgColor = Colors.blue,
     this.selectColor = Colors.white,
     this.selectBgColor = Colors.green,
     this.defaultSelect = 0,
     this.topRight,
     this.hasTopRight,
+    this.margin,
+    this.padding,
+    this.minWidth,
   });
 
   //数据
   final List<String> data;
 
   final Function onChange;
+  final double? fontSize;
   final double? size;
+  final double? minWidth;
   final Color? color;
   final Color? selectColor;
   final Color? bgColor;
@@ -29,6 +35,8 @@ class CxSelectButtonList extends StatefulWidget {
   final int? defaultSelect;
   final Widget? topRight;
   final List<int>? hasTopRight;
+  final EdgeInsetsGeometry? padding;
+  final EdgeInsetsGeometry? margin;
 
   @override
   State<CxSelectButtonList> createState() => _CxSelectButtonListState();
@@ -42,6 +50,7 @@ class _CxSelectButtonListState extends State<CxSelectButtonList> {
       height: widget.size ?? 60,
       color: widget.boxColor,
       child: ListView.builder(
+        // shrinkWrap: true,
         scrollDirection: Axis.horizontal,
         itemBuilder: itemBuilder,
         itemCount: widget.data.length,
@@ -50,36 +59,48 @@ class _CxSelectButtonListState extends State<CxSelectButtonList> {
   }
 
   Widget? itemBuilder(BuildContext context, int index) {
-    final stack = Stack(children: [
-      GestureDetector(
-        onTap: () {
-          widget.onChange(index, widget.data.elementAt(index));
-          setState(() {
-            selectIndex = index;
-          });
-        },
-        child: Stack(children: [
-          Container(
-            decoration: BoxDecoration(
-              color:
-                  selectIndex == index ? widget.selectBgColor : widget.bgColor,
-              borderRadius: const BorderRadius.all(
-                Radius.circular(5),
+    final stack = Stack(
+      children: [
+        GestureDetector(
+          onTap: () {
+            widget.onChange(index, widget.data.elementAt(index));
+            setState(() {
+              selectIndex = index;
+            });
+          },
+          child: Stack(
+            children: [
+              Container(
+                constraints: BoxConstraints(minWidth: widget.minWidth ?? 50),
+                decoration: BoxDecoration(
+                  color: selectIndex == index
+                      ? widget.selectBgColor
+                      : widget.bgColor,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(5),
+                  ),
+                ),
+                padding: widget.padding ??
+                    const EdgeInsets.symmetric(vertical: 0, horizontal: 16),
+                margin: widget.margin ?? const EdgeInsets.all(8),
+                width: widget.size,
+                child: Center(
+                  child: Text(
+                    widget.data[index],
+                    style: TextStyle(
+                      fontSize: widget.fontSize ?? 14,
+                      color: selectIndex == index
+                          ? widget.selectColor
+                          : widget.color,
+                    ),
+                  ),
+                ),
               ),
-            ),
-            margin: const EdgeInsets.all(8),
-            width: widget.size ?? 50,
-            child: Center(
-                child: Text(
-              widget.data[index],
-              style: TextStyle(
-                color: selectIndex == index ? widget.selectColor : widget.color,
-              ),
-            )),
+            ],
           ),
-        ]),
-      ),
-    ]);
+        ),
+      ],
+    );
 
     if (widget.topRight != null &&
         widget.hasTopRight != null &&
