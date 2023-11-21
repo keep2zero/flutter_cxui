c8b8e2339a8d85045eb3dc146f1d67579a6fa79f
keep2zero <towaywu@gmail.com>
2023-11-21 08:52:22 +0800

    fix: 修复提示问题

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_cxui/main.dart';
+import '../example/lib/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
