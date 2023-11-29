3a7f2e60178dc52142d0ab161e46919eb247a417
keep2zero <towaywu@gmail.com>
2023-11-29 23:08:43 +0800

    feat: add functionality to cxbutton

--- example/lib/pages/page_button.dart
+++ example/lib/pages/page_button.dart
@@ -1,17 +1,182 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_cxui/components/button/button.dart';
+import 'package:flutter_cxui/cxui.dart';
 
-void main() {
-  runApp(const PageButtonApp());
-}
+void main() => runApp(const PageButtonApp());
 
 class PageButtonApp extends StatelessWidget {
   const PageButtonApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const CxButton(),
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.purpleAccent[50],
+        appBar: AppBar(
+          backgroundColor: Colors.purple,
+          foregroundColor: Colors.white,
+          title: const Text("CxButton"),
+        ),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Container(
+              // color: Colors.black,
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  buildSolid(),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  buildIcon(),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  buildTextIcon(),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  buildSize(),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildSolid() {
+    return const CxCard(
+      title: "Default",
+      shadow: true,
+      radius: 5,
+      body: Row(
+        // mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          CxButton(
+            text: "hello",
+            // padding: EdgeInsets.all(10),
+          ),
+          CxButton(
+            text: "hello",
+            color: Colors.pink,
+            // padding: EdgeInsets.all(10),
+          ),
+          CxButton(
+            type: CxButtonType.fill,
+            text: "hello",
+            color: Colors.purple,
+            // padding: EdgeInsets.all(10),
+          ),
+          CxButton(
+            text: "world",
+            radius: 10,
+            type: CxButtonType.fill,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildIcon() {
+    return const CxCard(
+      shadow: true,
+      radius: 5,
+      title: "ICON",
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          CxButton(
+            color: Colors.pink,
+            icon: Icons.home_filled,
+            radius: 8,
+          ),
+          CxButton(
+            radius: 10,
+            type: CxButtonType.fill,
+            color: Colors.green,
+            icon: Icons.account_balance_rounded,
+          ),
+          CxButton(
+            type: CxButtonType.fill,
+            color: Colors.green,
+            icon: Icons.wechat,
+          ),
+          CxButton(
+            shadow: true,
+            iconSize: 30,
+            radius: 30,
+            color: Colors.purple,
+            type: CxButtonType.fill,
+            icon: Icons.whatshot,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildTextIcon() {
+    return const CxCard(
+      title: "Label and Icon",
+      shadow: true,
+      radius: 5,
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          CxButton(
+            text: "Wechat",
+            type: CxButtonType.fill,
+            icon: Icons.wechat,
+            color: Colors.green,
+          ),
+          CxButton(
+            text: "Location",
+            icon: Icons.location_on_outlined,
+          ),
+          CxButton(
+            text: "Home",
+            color: Colors.purple,
+            type: CxButtonType.fill,
+            icon: Icons.home,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildSize() {
+    return const CxCard(
+      title: "Size",
+      shadow: true,
+      hdSplit: true,
+      radius: 5,
+      body: Align(
+        child: Wrap(
+          spacing: 10,
+          runSpacing: 10,
+          children: [
+            CxButton(
+              text: "w 60",
+              width: 60,
+              type: CxButtonType.fill,
+            ),
+            CxButton(
+              text: "w 160",
+              width: 160,
+              type: CxButtonType.fill,
+            ),
+            CxButton(
+              text: "height 30",
+              height: 30,
+              color: Colors.amber,
+              padding: EdgeInsets.all(0),
+              type: CxButtonType.fill,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/components/button/button.dart
+++ lib/components/button/button.dart
@@ -1,21 +1,177 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
+/// Button Component
+///
+/// Button has two states that they are solid and fill. Set the value of type property.
+///
 class CxButton extends StatefulWidget {
-  const CxButton({super.key});
+  const CxButton({
+    super.key,
+    this.text,
+    this.icon,
+    this.type = CxButtonType.solid,
+    this.color = const Color.fromARGB(255, 50, 73, 245),
+    this.textColor,
+    this.iconColor,
+    this.radius = 20,
+    this.padding = const EdgeInsets.all(8),
+    this.iconSize,
+    this.textSize,
+    this.shadow = true,
+    this.width,
+    this.height,
+    this.onTap,
+  });
+
+  /// button width.
+  final double? width;
+
+  /// button height.
+  final double? height;
+
+  ///whether to enable box shadow. default true.
+  final bool shadow;
+
+  /// button padding. default 8.
+  final EdgeInsetsGeometry? padding;
+
+  /// border radius. default 20.
+  final double? radius;
+
+  /// button label.
+  final String? text;
+
+  /// icon data.
+  final IconData? icon;
+
+  ///icon size. default 16.
+  final double? iconSize;
+
+  /// text size. default 12.
+  final double? textSize;
+
+  /// button type. `fill` and `solid` are the two states of the button.
+  final CxButtonType? type;
+
+  /// button color. default is blue.
+  final Color? color;
+
+  /// the label color. default is white.
+  final Color? textColor;
+
+  /// the icon color. default is white.
+  final Color? iconColor;
+
+  /// click event.
+  final void Function()? onTap;
 
   @override
   State<CxButton> createState() => _CxButtonState();
 }
 
 class _CxButtonState extends State<CxButton> {
+  int index = 0;
+  Future? delay;
   @override
   Widget build(BuildContext context) {
-    return Directionality(
-      textDirection: TextDirection.ltr,
-      child: Container(
-        // onPressed: () {},
-        child: const Text("hello"),
+    const defcolor = Color.fromARGB(255, 255, 255, 255);
+    Color color = index == 0 ? widget.color! : widget.color!.withAlpha(180);
+
+    final diconColor = widget.iconColor ??
+        (widget.type == CxButtonType.fill ? defcolor : color);
+    final dtextColor = widget.textColor ??
+        (widget.type == CxButtonType.fill ? defcolor : color);
+
+    Color iconColor = index == 0 ? diconColor : diconColor.withAlpha(100);
+    Color textColor = index == 0 ? dtextColor : dtextColor.withAlpha(100);
+
+    List<BoxShadow> bslist = [];
+    if (index == 1 && widget.shadow) {
+      bslist.add(
+        BoxShadow(
+          color: color.withAlpha(180),
+          // offset: Offset(2, 2),
+          spreadRadius: 3,
+          blurRadius: 2,
+          blurStyle: BlurStyle.solid,
+        ),
+      );
+    }
+
+    final deco = BoxDecoration(
+      color: widget.type == CxButtonType.fill ? color : null,
+      border:
+          widget.type == CxButtonType.solid ? Border.all(color: color) : null,
+      borderRadius: BorderRadius.circular(widget.radius!),
+      boxShadow: bslist,
+    );
+
+    return Stack(
+      children: [
+        buildBody(deco, iconColor, textColor),
+
+        ////////////////////////////////
+        // Positioned.fill(
+        //   child:
+        // )
+      ],
+    );
+  }
+
+  void lockup() {
+    if (delay != null) return;
+    print("hello");
+    delay = Future.delayed(const Duration(milliseconds: 100)).whenComplete(() {
+      setState(() {
+        index = 0;
+        delay = null;
+      });
+    });
+  }
+
+  Widget buildBody(BoxDecoration deco, Color iconColor, Color textColor) {
+    return GestureDetector(
+      onTap: widget.onTap,
+      onTapDown: (detail) {
+        if (delay == null) {
+          setState(() {
+            index = 1;
+          });
+        }
+      },
+      onLongPress: () => lockup(),
+      onTapUp: (detail) => lockup(),
+      child: AnimatedContainer(
+        width: widget.width,
+        height: widget.height,
+        padding: widget.padding,
+        decoration: deco,
+        duration: const Duration(milliseconds: 100),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (widget.icon != null)
+              Icon(
+                widget.icon,
+                color: iconColor,
+                size: widget.iconSize ?? 16,
+              ),
+            if (widget.text != null || widget.icon != null)
+              const SizedBox(
+                width: 2,
+              ),
+            if (widget.text != null)
+              Text(
+                widget.text!,
+                style: TextStyle(
+                    color: textColor, fontSize: widget.textSize ?? 12),
+              ),
+          ],
+        ),
       ),
     );
   }
 }
+
+enum CxButtonType { fill, solid }
