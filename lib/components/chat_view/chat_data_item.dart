cd785785d74c054406212d112957ca5f17c29e62
Cindy Zou <cindyzou@CindydeMacBook-Pro.local>
2024-06-10 23:04:03 +0800

    feat: add json format to ChatDataItem

--- lib/components/chat_view/chat_data_item.dart
+++ lib/components/chat_view/chat_data_item.dart
@@ -10,5 +10,54 @@ class ChatDataItem {
   final String? type;
   final String? thumb;
   final String? ext;
-  const ChatDataItem({this.ciId, this.uid, this.avatar, this.name, this.message, this.time, this.thumb, this.type, this.url, this.size, this.ext});
+  const ChatDataItem({
+    this.ciId,
+    this.uid,
+    this.avatar,
+    this.name,
+    this.message,
+    this.time,
+    this.thumb,
+    this.type,
+    this.url,
+    this.size,
+    this.ext,
+  });
+
+  factory ChatDataItem.fromJson(Map<String, dynamic> data) {
+    return ChatDataItem(
+      ciId: data["ciId"],
+      uid: data["uid"],
+      avatar: data["avatar"],
+      name: data["name"],
+      message: data["message"],
+      time: data["time"],
+      thumb: data["thumb"],
+      type: data["type"],
+      url: data["url"],
+      size: data["size"],
+      ext: data["ext"],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "ciId": ciId,
+      "uid": uid,
+      "avatar": avatar,
+      "name": name,
+      "message": message,
+      "time": time,
+      "thumb": thumb,
+      "type": type,
+      "url": url,
+      "size": size,
+      "ext": ext,
+    };
+  }
+
+  @override
+  String toString() {
+    return "ciId: $ciId, uid: $uid";
+  }
 }
